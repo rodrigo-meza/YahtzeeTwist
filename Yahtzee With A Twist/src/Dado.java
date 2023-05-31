b170a8a123bf7211db562e87360983ca1158cbd7
rodrigo.meza <rodrigo.meza72@uabc.edu.mx>
2023-05-30 23:34:57 -0700

    fix dice constructor

--- Yahtzee With A Twist/src/Dado.java
+++ Yahtzee With A Twist/src/Dado.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public abstract class Dado {
     protected int sides;
     protected int value;
-    protected ArrayList<String> colors;
+    protected ArrayList<String> colors = new ArrayList<>();
     protected HashMap<String,Integer> assignedValues = new HashMap<>();
     protected ArrayList<Integer> possibleValues = new ArrayList<>();
     protected int xpos,ypos;
@@ -26,7 +26,7 @@ public abstract class Dado {
             possibleValues.add(i);
         }
         for(int i = 0; i<sides ; i++){
-            int valueToAssign = possibleValues.get(rnd.nextInt(sides));
+            int valueToAssign = possibleValues.get(rnd.nextInt(possibleValues.size()));
             String colorToAssign = colors.get(rnd.nextInt(colors.size()));
             assignedValues.put(colorToAssign, valueToAssign);
             possibleValues.remove((Integer)valueToAssign);
