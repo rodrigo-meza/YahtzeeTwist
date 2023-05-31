2f00d52e327b1469b429440aa5fb0bf3bd503296
rodrigo.meza <rodrigo.meza72@uabc.edu.mx>
2023-05-30 23:44:21 -0700

    add hashmap for shape values

--- Yahtzee With A Twist/src/DadoDeFiguras.java
+++ Yahtzee With A Twist/src/DadoDeFiguras.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DadoDeFiguras extends Dado{
-    
+    private HashMap<Integer,String> shapes = new HashMap<>();
     public DadoDeFiguras(int x, int y) {
         super(x, y);
-
+        shapes.put(1, "circle");
+        shapes.put(2, "square");
+        shapes.put(3, "rectangle");
+        shapes.put(4, "triangle");
+        shapes.put(5, "pentagon");
+        shapes.put(6, "rhombus");
     }
-
     @Override
     public void showSide() {
-        
+
     }
-    
+
 }
